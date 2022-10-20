4b88a1bbee7837049f8c29e0ae901078af27ca8a
“Douglas <douglasbermudez.alvarado@gmail.com>
2022-10-20 02:43:21 -0400

    Temporary changes

--- translator.py
+++ translator.py
@@ -1,44 +1,41 @@
 """translator.py """
 import json
-from ibm_watson import LanguageTranslatorV3
-from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
 from dotenv import load_dotenv
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 load_dotenv()
 
 apikey = os.environ['apikey']
-authenticator = IAMAuthenticator(apikey)
-language_translator = LanguageTranslatorV3(
-                        version='2018-05-01',
-                        authenticator=authenticator
-                        )
-language_translator.set_service_url
-
 #url = os.environ['url']
 url = os.environ['https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/1ba4b7f1-a462-4dfd-b5af-68ad0ee6fce1']
 
-def englishToFrench(englishText):
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+language_translator.set_service_url
+language_translator.set_disable_ssl_verification(True)
+
+def english_to_french(englishText):
     """ 
     This function traslates english to french
     """
     #write the code here
     frenchtranslation = language_translator.translate(
-                                text=englishText,
-                                model_id='en-fr'
-                            ).get_result()
+    text=englishText,
+    model_id='en-fr').get_result()
+    frenchText=frenchtranslation.get("translations")[0].get("translation")
+    return frenchText 
 
-    #return frenchText 
-    return frenchtranslation.get("translations")[0].get("translation")
-
-def frenchToEnglish(frenchText):
+def french_to_english(frenchText):
     """ 
     This function traslates english to french
     """
     #write the code here
     englishtranslation = language_translator.translate(
-                            text=frenchText, model_id='en-fr'
-                            ).get_result()
-
-    #return frenchText 
-    return englishtranslation.get("translations")[0].get("translation")
+    text=frenchText, model_id='en-fr').get_result()
+    englishText=englishtranslation.get("translations")[0].get("translation")
+    return englishText 
