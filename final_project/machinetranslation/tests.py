2dc2c4ea5f6daea8dea2e3cf6f635a393ba55d28
“Douglas <douglasbermudez.alvarado@gmail.com>
2022-10-27 01:52:14 -0400

    Temporary changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,25 +1,28 @@
 """ Test functions """
+#import pdb
 import unittest
 from translator import english_to_french
 from translator import french_to_english
 
-class TestEnglis2French(unittest.TestCase):
+#class TestEnglis2French(unittest.TestCase):
+class TestModule(unittest.TestCase):    
     """ English to French."""
-    def test_e2f1(self):
+    def test_e2f(self):
         #self.assertEqual(english_to_french('Hello'), 'Bonjour') #E2F
         self.assertEqual(english_to_french('Hello'), 'Bonjour') #E2F
-    def test_e2f_bonjour1(self):
+    def test_e2f2(self):
         self.assertNotEqual(english_to_french(0), 0)            #Null
-    def test_e2f_bonjour2(self):
+    def test_e2f3(self):
         self.assertNotEqual(english_to_french('None'), ' ')     #Null
-
-class TestFrench2English(unittest.TestCase):
+#pdb.set_trace()
+#class TestFrench2English(unittest.TestCase):
+class TestModule1(unittest.TestCase):
     """ french to english """
-    def test_f2e1(self):
+    def test_f2e(self):
         self.assertEqual(french_to_english('Bonjour'), 'Hello')  #F2E
-    def test_f2e_bonjour1(self):
+    def test_f2e2(self):
         self.assertNotEqual(french_to_english(0), 0)             #Null
-    def test_f2e_bonjour2(self):
+    def test_f2e3(self):
         self.assertNotEqual(french_to_english('None'), ' ')      #Null
 if __name__=="__main__":
     pass
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,34 +1,32 @@
 """
 translator.py
 """
-#import json
+import json
 import os
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-
 from dotenv import load_dotenv
+
 load_dotenv()
 
-apikey = os.environ['apikey']
-url = os.environ['url']
+#apikey = os.environ['apikey']
+#url = os.environ['url']
 #apikey = "DYl4fdske7WTuUZc6Rl-UOmseVMzI1KLv8nW8MnyhOak"
 #authenticator = IAMAuthenticator(apikey)
-authenticator = IAMAuthenticator('_RtomCmW3LrrujUnNKRVeQYk_9ewz1QP6Fctcvxi9cxg')
-language_translator = LanguageTranslatorV3(
-                        version='2018-05-01',
-                        authenticator=authenticator
-                        )
+authenticator = IAMAuthenticator('C-ile0-MsqC5aVdBEDeOxjEQ9iLvzOk2cxmilhbjrSeR')
+language_translator = LanguageTranslatorV3(version='2018-05-01',authenticator=authenticator)
+
 #language_translator.set_service_url(url)
-language_translator.set_service_url('https://api.us-south.natural-language-understanding.watson.cloud.ibm.com/instances/1ba4b7f1-a462-4dfd-b5af-68ad0ee6fce1')
+language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/1f00eb3a-248e-4e01-bebf-0504bc3752de')
 #language_translator.set_disable_ssl_verification(True)
 
 def english_to_french(englishtext):
     """
     conver englist to french
     """
-    translation = language_translator.translate(text=englishtext, model_id='en-fr').get_result()
+    translation= language_translator.translate(text=englishtext, model_id='en-fr').get_result()
     #return frenchtranslation.get("translations")[0].get("translation")
-    frenchtext = translation['translations'][0].get('translation')
+    frenchtext = translation['translations'][0]['translation']
     return frenchtext
 
 def french_to_english(frenchtext):
